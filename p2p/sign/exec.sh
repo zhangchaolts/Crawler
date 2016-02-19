ed8cc82b0eed45b790e7c46eb0d0b235ac968808
zhangchaolts <43228637@qq.com>
2016-02-19 20:54:11 +0800

    modify huirendai

--- p2p/sign/exec.sh
+++ p2p/sign/exec.sh
@@ -4,3 +4,5 @@ python minxindai.py 1>>minxindai.log 2>>minxindai.err
 
 python jinronggongchang.py 1>>jinronggongchang.log 2>>jinronggongchang.err
 php jinronggongchang.php
+
+python huirendai.py 1>>huirendai.log 2>>huirendai.err
